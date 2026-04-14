a1af3f4a8b10b45e4aa6a5a840dd4be83fa99a4e
Sulaimon <sulaimonwasiu13@gmail.com>
2026-04-14 04:42:55 +0100

    sample api

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-def add(a, b):
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def add(a: int, b: int) -> int:
     return a + b
 
-if __name__ == "__main__":
-    print(add(2, 3))
+@app.get("/")
+def read_root():
+    return {"message": "Hello CI/CD 🚀"}
+
+@app.get("/add/{a}/{b}")
+def add_endpoint(a: int, b: int):
+    result = add(a, b)
+    return {"result": result}
